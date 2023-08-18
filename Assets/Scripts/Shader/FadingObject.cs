e2919404e4ebe02d14caca96604b3c0e2f56c4c8
MatteoSciutteri <thejester79@gmail.com>
2023-08-18 03:39:11 +0200

    Shadow system completed (mio dio che fatica)

---
+++ Assets/Scripts/Shader/FadingObject.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FadingObject : MonoBehaviour, IEquatable<FadingObject>
+{
+    public List<Renderer> Renderers = new List<Renderer>();
+    public Vector3 Position;
+    public List<Material> Materials = new List<Material>();
+    [HideInInspector]
+    public float InitialAlpha;
+    public float InitialOpacity;
+
+    private void Awake()
+    {
+        Position = transform.position;
+
+        if (Renderers.Count == 0)
+        {
+            Renderers.AddRange(GetComponentsInChildren<Renderer>());
+        }
+        foreach(Renderer renderer in Renderers)
+        {
+            Materials.AddRange(renderer.materials);
+        }
+
+        //InitialAlpha = Materials[0].color.a;
+        InitialOpacity = Materials[0].GetFloat("_Opacity");
+    }
+
+    public bool Equals(FadingObject other)
+    {
+        return Position.Equals(other.Position);
+    }
+
+    public override int GetHashCode()
+    {
+        return Position.GetHashCode();
+    }
+}
